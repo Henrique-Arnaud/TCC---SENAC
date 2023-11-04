c5e946a299df4e6967ee9d0ac4db2c871dcffa67
Henrique-Arnaud <henriquearnauds@gmail.com>
2023-11-04 15:57:05 -0300

    closed eyes moved to 'fechado' folder

--- copyItems.py
+++ copyItems.py
@@ -1,15 +1,15 @@
 import shutil
 import os
 
-targetPath = r'./baseOficial/600img/cinza/50x50/abertoTreino/'
-destinationPath = r'./baseOficial/600img/cinza/50x50/abertoRenomeado/'
+targetPath = r'./baseOficial/600img/colorida/50x50/'
+destinationPath = r'./baseOficial/600img/colorida/50x50/fechado/'
 
 mylist = os.listdir(targetPath)
 mylist.sort()
 count = 0
 for index, file_name in enumerate(mylist):
   source = targetPath + file_name
-  destination = destinationPath + 'aberto' + str(index) + '.jpg'
+  destination = destinationPath + 'fechado' + str(index) + '.jpg'
     # copy only files
   if os.path.isfile(source):
     shutil.move(source, destination)
